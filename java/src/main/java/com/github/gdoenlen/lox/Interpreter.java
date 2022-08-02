7b189678e7b52abe4cf8e3f516ae9bd03a307e68
George Doenlen <gdoenlen@gmail.com>
2022-08-02 10:44:13 -0400

    more interpeting

--- java/src/main/java/com/github/gdoenlen/lox/Interpreter.java
+++ java/src/main/java/com/github/gdoenlen/lox/Interpreter.java
@@ -1,8 +1,8 @@
 package com.github.gdoenlen.lox;
 
-import static com.github.gdoenlen.lox.TokenType.MINUS;
+import java.util.Objects;
 
-public class Interpreter {
+class Interpreter {
     Object interpret(Expr expr) {
         return switch (expr) {
             case Literal l -> l.value();
@@ -34,14 +34,43 @@ public class Interpreter {
 
     private Object binary(Binary b) {
         TokenType type = b.operator().tokenType();
-        var left = (double) this.interpret(b.left());
-        var right = (double) this.interpret(b.right());
+        Object left = this.interpret(b.left());
+        Object right = this.interpret(b.right());
 
         return switch (type) {
-            case MINUS -> left - right;
-            case SLASH -> left / right;
-            case STAR -> left * right;
+            case BANG_EQUAL -> !Objects.equals(left, right);
+            case EQUAL_EQUAL -> Objects.equals(left, right);
+            case GREATER -> (Double) left > (Double) right;
+            case GREATER_EQUAL -> (Double) left >= (Double) right;
+            case LESS -> (Double) left < (Double) right;
+            case LESS_EQUAL -> (Double) left <= (Double) right;
+            case MINUS -> (Double) left - (Double) right;
+            case PLUS -> plus(left, right);
+            case SLASH -> (Double) left / (Double) right;
+            case STAR -> (Double) left * (Double) right;
             default -> throw new IllegalArgumentException(type.toString());
         };
+    }
+
+    private static Object plus(Object left, Object right) {
+        if (left instanceof String l) {
+            return l + right;
+        }
+
+        if (right instanceof String r) {
+            return r + left;
+        }
+
+        if (left instanceof Double l && right instanceof Double r) {
+            return l + r;
+        }
+
+        throw new IllegalArgumentException(
+            String.format(
+                "Tried to add or concatenate non-string or non-double classes: %s, %s",
+                left.getClass(),
+                right.getClass()
+            )
+        );
     }
 }
