6bf6df97990c5946be5bbd75a1b590572da6950a
George Doenlen <gdoenlen@gmail.com>
2024-09-08 18:12:47 -0700

    package private ziperator

--- java/src/main/java/com/github/gdoenlen/lox/util/Ziperator.java
+++ java/src/main/java/com/github/gdoenlen/lox/util/Ziperator.java
@@ -4,12 +4,12 @@ import java.util.Iterator;
 import java.util.function.BiFunction;
 
 /** Iterator to zip two iterators into one. Assumes they will be the same size. */
-public class Ziperator<A, B, T> implements Iterator<T> {
+class Ziperator<A, B, T> implements Iterator<T> {
     private final Iterator<A> a;
     private final Iterator<B> b;
     private final BiFunction<A, B, T> mapper;
 
-    public Ziperator(Iterator<A> a, Iterator<B> b, BiFunction<A, B, T> mapper) {
+    Ziperator(Iterator<A> a, Iterator<B> b, BiFunction<A, B, T> mapper) {
         this.a = a;
         this.b = b;
         this.mapper = mapper;
